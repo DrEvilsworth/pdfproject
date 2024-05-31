d9df46758efb727236c8614258406c8d03d0c136
nathan-barron <nathan.barron@employbridge.com>
2024-05-31 13:40:32 -0400

    changed from wile to for and made colors random

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fpdf import FPDF
 import pandas as pd
+import random
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 
@@ -8,17 +9,19 @@ df = pd.read_csv('topics.csv',  sep=',')
 for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font('Times', style='B', size=24)
-    pdf.set_text_color(100, 100, 100)
+    pdf.set_text_color(random.randint(0,255), random.randint(0,255), random.randint(0,255))
     #pdf.cell(w=0, h=12, txt=row["Topic"], align='L', ln=1, border='B')
     pdf.cell(w=0, h=12, txt=row["Topic"], align='L', ln=1)
     pdf.line(10, 21, 200, 21)
     num_pages = row['Pages'] - 1
-    while True:
-        if num_pages == 0:
-            break
-        else:
-            pdf.add_page()
-            num_pages = num_pages - 1
+    #while True:
+    #    if num_pages == 0:
+    #        break
+    #    else:
+    #        pdf.add_page()
+    #        num_pages = num_pages - 1
+    for i in range(row['Pages'] - 1):
+        pdf.add_page()
 
 
 pdf.output('output.pdf')
