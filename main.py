5ee9396a8ca4f88452442d6f4ac38e73da52d490
nathan-barron <nathan.barron@employbridge.com>
2024-05-31 13:29:03 -0400

    added all pages and headers

---
+++ main.py
@@ -0,0 +1,24 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+
+df = pd.read_csv('topics.csv',  sep=',')
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font('Times', style='B', size=24)
+    pdf.set_text_color(100, 100, 100)
+    #pdf.cell(w=0, h=12, txt=row["Topic"], align='L', ln=1, border='B')
+    pdf.cell(w=0, h=12, txt=row["Topic"], align='L', ln=1)
+    pdf.line(10, 21, 200, 21)
+    num_pages = row['Pages'] - 1
+    while True:
+        if num_pages == 0:
+            break
+        else:
+            pdf.add_page()
+            num_pages = num_pages - 1
+
+
+pdf.output('output.pdf')
